4c2b83c7b9545b2d35c7c984e76777838cdd6232
Erin Dunn <erindunn@live.co.uk>
2019-11-27 13:16:48 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  components:{
+    appHeader: Header
+  },
+  created(){
+    this.$store.dispatch('initStocks')
+  }
+  
+
+}
+</script>
+
+<style>
+body {
+margin-top:30px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Trade or view Portfolio</h1>
+        <h2>You may Save and Load your data</h2>
+        <h2>Click End Day to progress to the next day.</h2>
+        <hr>
+        <p>Your Funds: {{funds}}</p>
+    </div>
+</template>
+<script>
+    export default {
+        computed:{
+            funds(){
+                return this.$store.getters.funds
+            }
+        }
+    }
+</script>
+<style scoped>
+h2{
+    font-size:2rem;
+}
+</style>
